7908a278b35e5010528bb4abf178772a0da3824a
Saara <jonessaara@gmail.com>
2025-07-25 15:20:53 +0200

    hit em

--- Survivor/characters/enemies/troll/troll.gd
+++ Survivor/characters/enemies/troll/troll.gd
@@ -9,9 +9,14 @@ func _ready() -> void:
 
 func _on_target_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Bullet"):
-		health.value -= 20
+		health.value -= 50
 
 
 func _on_progress_bar_value_changed(value: float) -> void:
 	if value <= 0:
 		queue_free()
+
+
+func _on_target_area_area_entered(area: Area2D) -> void:
+	if area.name == "HitArea":
+		health.value -= 50
