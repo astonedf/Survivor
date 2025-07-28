01f40549538f8f32677b7589988556671b3b2448
Dherleth <erwan.moreira@gmail.com>
2025-07-28 15:33:16 +0200

    remoed hit animation from player
    This was not neccessary anymore, since weapons are separeted from player

--- Survivor/characters/player/player.gd
+++ Survivor/characters/player/player.gd
@@ -3,21 +3,18 @@ class_name Player extends Character
 @onready var bullet = preload("res://weapons/bullet/bullet.tscn")
 @onready var witch_arm: Weapon = $WitchArm
 @onready var witch_broom: Weapon = $WitchBroom
-@onready var hit_timer: Timer = $HitTimer
 
 var flower_in_range = []
 var enemy_in_range = false
 var destination: Vector2
 var target_position: Vector2
 var direction
-var attacking_animation_playing: bool = false
 	
 
 func _ready() -> void:
 	super._ready()
 	witch_arm.pickup(self)
 	witch_broom.pickup(self)
-	hit_timer.start()
 	
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -49,13 +46,14 @@ func _physics_process(delta):
 		
 		handle_movement_anim(direction)
 			
+			
 func handle_movement_anim(dir):
-	if !attacking_animation_playing:
-		if !velocity:
-			$AnimationPlayer.play("Idle")
-		if velocity:
-			$AnimationPlayer.play("Walk")
-			toggle_flip_sprite(dir)
+	if !velocity:
+		$AnimationPlayer.play("Idle")
+	if velocity:
+		$AnimationPlayer.play("Walk")
+		toggle_flip_sprite(dir)
+
 
 func toggle_flip_sprite(dir):
 	if dir == 1:
@@ -70,16 +68,6 @@ func shoot():
 	instance.spawnPos = global_position
 	instance.spawnRot = $Spray.rotation
 	get_parent().add_child.call_deferred(instance)
-	
-
-func _on_hit_timer_timeout() -> void:
-	attacking_animation_playing = true
-	$AnimationPlayer.play("Hit")
-
-
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "Hit":
-		attacking_animation_playing = false
 
 
 func _on_weapon_pickup_range_area_body_entered(weapon: Weapon) -> void:
