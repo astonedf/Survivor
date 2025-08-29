986c5e5539edcbc1284c546ecc0219f75cbe5862
Dherleth <erwan.moreira@gmail.com>
2025-08-29 17:47:55 +0200

    merged skill_tree

--- Survivor/components/xp_blood/xp_blood.gd
+++ Survivor/components/xp_blood/xp_blood.gd
@@ -3,7 +3,7 @@ class_name XpBlood extends Node2D
 @export_custom(PROPERTY_HINT_NONE, "suffix:s") var collecting_time := 5
 @export_custom(PROPERTY_HINT_NONE, "suffix:xp") var xp := 0
 @export_range(0, 100, 1, "suffix:%")  var _drop_chance := 0
-const pickup_scene: PackedScene = preload("res://characters/xp_blood/xp_blood_pickup.tscn")
+const pickup_scene: PackedScene = preload("res://components/xp_blood/xp_blood_pickup.tscn")
 
 
 func spawn(pickup_position: Vector2) -> void:
